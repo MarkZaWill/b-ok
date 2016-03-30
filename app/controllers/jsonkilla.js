d21208ccb384e340be9f915c3b7ae57eb509448a
Mark Zachary <MarkZaWill@gmail.com>
2016-03-30 11:26:53 -0500

    made everything work.  All I need to do now is change the yellow font color so it isn't as hard to read.

--- app/controllers/jsonkilla.js
+++ app/controllers/jsonkilla.js
@@ -13,7 +13,9 @@ MarkApp.controller("jsonkilla",
  
 
   function ($scope, $routeParams, $location, $http, FireFactory, authFactory, $firebaseArray) {
-  		
+  	
+    let user = authFactory.getUser();
+      	
     // Default properties for bound variables
     $scope.copingSkills = [];
     $scope.selectedSkills = {};
@@ -58,19 +60,26 @@ MarkApp.controller("jsonkilla",
     //  };
      $scope.deleteSkill = function(rawCopingSkill){ 
       console.log("id", rawCopingSkill);
-      $scope.rawCopingSkills.$remove(rawCopingSkill).then(() => $location.url("/"));}   
+      $scope.rawCopingSkills.$remove(rawCopingSkill).then(() => $location.url("#/mood/"));}   
      // $scope.deleteSkill = () => $http
      //    .delete(`https://be-ok.firebaseio.com/copingSkills/${$routeParams.skillsId}.json`)
      //    .then(() => $location.url("/"));
    
-    $scope.updateSkill = function(rawCopingSkill) {
-    console.log(rawCopingSkill);
-      $scope.rawCopingSkills.$save(rawCopingSkill).then(() => $location.url("/"));
-      console.log("$scope.rawCopingSkills", $scope.rawCopingSkills);}
+    // $scope.updateSkill = function(rawCopingSkill) {
+    // console.log(rawCopingSkill);
+    //   $scope.rawCopingSkills.$save(rawCopingSkill).then(() => $location.url("/"));
+    //   console.log("$scope.rawCopingSkills", $scope.rawCopingSkills);}
 
    $scope.addSkill = function (newSkill) {
+    let user = authFactory.getUser();
+      newSkill = {
+        skill: $scope.newSkill.skill,
+        uid: user.uid
+      };
     console.log("this was clicked");
-      $scope.rawCopingSkills.$add(newSkill);}
+      $scope.rawCopingSkills.$add(newSkill).then(()=> $location.url("#/mood/"));}
+
+
 
     // $http
     //     .$save(`https://be-ok.firebaseio.com/copingSkills/${$routeParams.skillid}.json`)
--- app/factories/WeatherFactory.js
+++ app/factories/WeatherFactory.js
@@ -1,24 +1,53 @@
+'use strict';
 
-"use strict";
+$(function() {
 
-MarkApp.factory("WeatherFactory", ($q, $http, authFactory, firebaseURL) =>
-   () =>
-    $q((resolve, reject) => { 
-      let user = authFactory.getUser();
-      console.log("user.uid", user.uid);
-        $http
-        .get(`${firebaseURL}copingSkills.json`)
-        .success(
-          copeGroup => 
-          	{resolve(copeGroup)
-          		console.log("SUCCESS", copeGroup);},
-          error => reject(error)
-        );
-      })
+  //This begins the API Weather integration that changes the background and adds the current conditions to the footer.
 
-);
+//these are the ids that are being pulled from the API to help to change the backgrounds.
+  var sunnyWeather = [800, 801, 802, 803, 804];
+  var snowyWeather = [600, 601, 602, 611, 612, 615, 616, 620, 621, 622];
+  var rainyWeather = [300, 301, 302, 310, 311, 312, 313, 314, 321, 500, 501, 502, 503, 504, 511, 520, 521, 522, 531];
+  var stormyWeather = [200, 201, 202, 210, 211, 212, 221, 230, 231, 232];
+  var foggyWeather = [701, 711, 721, 731, 741, 751, 761, 762, 771, 781];
+  var extremeWeather = [900, 901, 902, 903, 904, 905, 906];
+  //This is the call for the API. The callback function here includes the test to change the background
+  
+  $.ajax({
+    url: 'http://api.openweathermap.org/data/2.5/weather?id=5003243&appid=d3bd75156e1363b7b3c8e43c98744398',
+    dataType: 'jsonp',
+    success: function(results) {
+      var currentWeather = results.weather[0].id;
+      console.log("currentWeather", results.weather[0].id);
+      var currentTemp = results.main.temp - 273;
+      if (isItInArray(currentWeather, sunnyWeather)) {
+        $('body').addClass('sunny');
+        console.log("it is sunny");
+      } else if (isItInArray(currentWeather, foggyWeather)) {
+        $('body').addClass('fog');
+        console.log("it is foggy");
+      } else if (isItInArray(currentWeather, rainyWeather)) {
+        $('body').addClass('rainy');
+        console.log("it is rainy");
+      } else if (isItInArray(currentWeather, stormyWeather)) {
+        $('body').addClass('stormy');
+        console.log("it is stormy");
+      } else if (isItInArray(currentWeather, snowyWeather)) {
+        $('body').addClass('snow');
+        console.log("it is snowy");
+      } else if (isItInArray(currentWeather, extremeWeather)) {
+        $('body').addClass('extreme');
+        console.log("it is deadly");
+      }
+    }
+  });
 
+  //check to see if item is in array
+  function isItInArray(value, array) {
+    return $.inArray(value, array) > -1;
+  }
 
+});
 
 
 // weather.main.id=
